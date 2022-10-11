b7f4d91378acc1bce00578ec75ca3c515dd9c3a4
ArcanusNEO <lucasding@foxmail.com>
2022-10-12 01:16:09 +0800

    fix

--- inc/winchat.h
+++ inc/winchat.h
@@ -8,9 +8,10 @@
 
 #define BACKLOG_SZ    20
 #define MX_MSG_SZ     2048
-#define MX_MSG_BUF_SZ (MX_MSG_SZ + 256)
+#define MX_TIME_SZ    256
+#define MX_MSG_BUF_SZ (MX_MSG_SZ + MX_TIME_SZ)
 
-const char conn_alert[] = "==> session established";
+const char conn_alert[] = "==> session begin";
 
 int parse_arg(int argc, char* argv[], char** ip_str, int* port) {
   if (argc != 2 && argc != 3) return 1;
@@ -21,42 +22,47 @@ int parse_arg(int argc, char* argv[], char** ip_str, int* port) {
 }
 
 void exec_chat(SOCKET sock) {
-  static char msg[MX_MSG_BUF_SZ];
+  static char send_buf[MX_MSG_BUF_SZ];
   int         eol = 1;
   while (1) {
     if (eol) printf("-> ");
-    fgets(msg, MX_MSG_SZ, stdin);
+    fgets(send_buf, MX_MSG_SZ, stdin);
     eol = 1;
-    if (msg[0] == '\n') puts("==> no message to send");
+    if (send_buf[0] == '\n') puts("==> no message to send");
     else {
-      size_t len = strlen(msg);
-      if (msg[len - 1] != '\n') {
-        msg[len++] = '\n';
-        msg[len]   = '\0';
-        eol        = 0;
+      size_t len = strlen(send_buf);
+      if (send_buf[len - 1] != '\n') {
+        send_buf[len++] = '\n';
+        send_buf[len]   = '\0';
+        eol             = 0;
       }
-      strcat(msg, "==> time: ");
+      strcat(send_buf, "==> time: ");
       time_t t = time(NULL);
-      strcat(msg, ctime(&t));
-      printf("%s", msg + len);
-      send(sock, msg, strlen(msg) + 1, 0);
+      strcat(send_buf, ctime(&t));
+      printf("%s", send_buf + len);
+      send(sock, send_buf, strlen(send_buf) + 1, 0);
     }
 
-    memset(msg, 0, sizeof(msg));
-    int first_alert = 1;
+    static char recv_buf[MX_MSG_BUF_SZ];
+    int         first_alert = 1;
     while (1) {
-      if (recv(sock, msg, MX_MSG_BUF_SZ, 0) <= 0) break;
+      int ret = recv(sock, recv_buf, MX_MSG_BUF_SZ, 0);
+      if (ret < 0) break;
+      else if (ret == 0) {
+        puts("==> session end");
+        return;
+      }
       static const char new_msg_alert[] = "==> new message";
       if (first_alert) {
         puts(new_msg_alert);
         first_alert = 0;
       }
-      for (char* i = msg; i < msg + MX_MSG_BUF_SZ; ++i) {
+      for (char* i = recv_buf; i < recv_buf + MX_MSG_BUF_SZ; ++i) {
         if (*i) {
           putchar(*i);
           *i = '\0';
         } else {
-          if (i + 1 >= msg + MX_MSG_BUF_SZ || *(i + 1) == '\0') break;
+          if (i + 1 >= recv_buf + MX_MSG_BUF_SZ || *(i + 1) == '\0') break;
           puts(new_msg_alert);
         }
       }
